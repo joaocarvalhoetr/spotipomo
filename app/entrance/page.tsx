50141ef9c1d5a9c27ca3d02f63010a29a01f36e9
JC <joaocarvalhoetr@gmail.com>
2024-10-01 18:55:56 +0100

    ♻️ (page.tsx): remove unused Image import to clean up code and improve readability ♻️ (page.tsx): remove unused useEffect import to streamline component dependencies

--- app/entrance/page.tsx
+++ app/entrance/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [isLogin, setIsLogin] = useState(true);
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center w-full min-h-screen">
